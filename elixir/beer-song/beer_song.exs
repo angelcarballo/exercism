c9ad51d02fdf948c782014627239b6b0b7866c97
Angel Carballo <contact@angelcarballo.com>
2019-07-28 13:55:48 +0100

    Elixir - Beer song (second iteration)

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,8 +3,17 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(1) do
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" <>
+    "Take it down and pass it around, no more bottles of beer on the wall.\n"
+  end
+  def verse(0) do
+    "No more bottles of beer on the wall, no more bottles of beer.\n" <>
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  end
   def verse(number) do
-    "#{bottles_on_wall(number)}\n#{take_one(number)}\n"
+    "#{number} bottles of beer on the wall, #{number} #{pluralize("bottle", number)} of beer.\n" <>
+      "Take one down and pass it around, #{number-1} #{pluralize("bottle", number-1)} of beer on the wall.\n"
   end
 
   @doc """
@@ -19,26 +28,6 @@ defmodule BeerSong do
   end
   def lyrics(), do: lyrics(99..0)
 
-  defp bottles_on_wall(number) when number > 1 do
-    "#{number} bottles of beer on the wall, #{number} bottles of beer."
-  end
-  defp bottles_on_wall(1) do
-    "1 bottle of beer on the wall, 1 bottle of beer."
-  end
-  defp bottles_on_wall(0) do
-    "No more bottles of beer on the wall, no more bottles of beer."
-  end
-
-  defp take_one(number) when number > 2 do
-    "Take one down and pass it around, #{number-1} bottles of beer on the wall."
-  end
-  defp take_one(2) do
-    "Take one down and pass it around, 1 bottle of beer on the wall."
-  end
-  defp take_one(1) do
-    "Take it down and pass it around, no more bottles of beer on the wall."
-  end
-  defp take_one(0) do
-    "Go to the store and buy some more, 99 bottles of beer on the wall."
-  end
+  defp pluralize(word, 1), do: word
+  defp pluralize(word, count) when count > 1, do: word <> "s"
 end
