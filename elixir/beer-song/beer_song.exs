b0e098cd5d38523586c545ac4867829c2bd419b5
Angel Carballo <contact@angelcarballo.com>
2019-07-28 21:12:01 +0100

    Elixir - Beer song (3rd iteration)

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,6 +3,14 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(number) when number > 2 do
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" <>
+      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+  end
+  def verse(2) do
+    "2 bottles of beer on the wall, 2 bottles of beer.\n" <>
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+  end
   def verse(1) do
     "1 bottle of beer on the wall, 1 bottle of beer.\n" <>
     "Take it down and pass it around, no more bottles of beer on the wall.\n"
@@ -11,23 +19,14 @@ defmodule BeerSong do
     "No more bottles of beer on the wall, no more bottles of beer.\n" <>
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
-  def verse(number) do
-    "#{number} bottles of beer on the wall, #{number} #{pluralize("bottle", number)} of beer.\n" <>
-      "Take one down and pass it around, #{number-1} #{pluralize("bottle", number-1)} of beer on the wall.\n"
-  end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
+  def lyrics(range \\ 99..0) do
     range
-    |> Enum.to_list
     |> Enum.map(&verse/1)
     |> Enum.join("\n")
   end
-  def lyrics(), do: lyrics(99..0)
-
-  defp pluralize(word, 1), do: word
-  defp pluralize(word, count) when count > 1, do: word <> "s"
 end
